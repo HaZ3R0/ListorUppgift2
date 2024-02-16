20e538aae4d1f9103bfaadb89566e093081057cc
HaZ3R0 <megamackan@outlook.com>
2024-02-16 12:25:40 +0100

    Add project files.

---
+++ ListorUppgift2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+namespace ListorUppgift2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> lista = new List<int>();
+            lista.Add(1);
+            lista.Add(2);
+            lista.Add(3);
+            Max(lista);
+
+        }
+
+        static void Max(List<int>lista)
+        {
+            lista.Sort();
+            lista.RemoveRange (0, lista.Count -1);
+            lista.Remove(1);
+        
+            foreach (int item in lista)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
